48fa363c2aef6411325d51cce54ada593821f523
kshatriya1502 <kshatriyaaditya1502@gmail.com>
2024-10-21 20:14:26 +0530

    commit

---
+++ Lec2.java
@@ -0,0 +1,31 @@
+//DATA TYPES IN THE JAVA :
+
+public class Lec2 {
+    public static void main(String[] args) {
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+        System.out.println(myNum);
+
+
+//        Primitive Data Types
+//        A primitive data type specifies the size and
+//        type of variable values, and it has no additional methods.
+        /*
+        * Primitive data types are simple, predefined data types that are
+        * used to store basic data. They are the building blocks of data
+        * manipulation  and cannot be further divided into simpler data types.
+
+        *
+        * */
+
+
+//
+//        There are eight primitive data types in Java:
+        // byte , short , int , long , float , double , boolean , char
+
+    }
+}
